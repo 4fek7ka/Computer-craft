ac1dfbe538ce3a31eaa78ed27ec4db9bb1a88fb6
Illia <htechakaka@gmail.com>
2026-03-27 00:54:25 +0100

    make asuto download all scripts script

---
+++ install.lua
@@ -0,0 +1,22 @@
+local user = "4fek7ka"
+local repo = "Computer-craft"
+local branch = "main"
+
+local files = {
+  { remote = "scripts/replace.lua", localName = "replace.lua" },
+  { remote = "scripts/PowahOneItem.lua", localName = "PowahOneItem.lua" }
+}
+
+for _, file in ipairs(files) do
+  local url = "https://raw.githubusercontent.com/" ..
+    user .. "/" .. repo .. "/" .. branch .. "/" .. file.remote
+
+  if fs.exists(file.localName) then
+    fs.delete(file.localName)
+  end
+
+  print("Downloading: " .. file.localName)
+  shell.run("wget", url, file.localName)
+end
+
+print("INSTALL COMPLETE")
